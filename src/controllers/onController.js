4f08bc23736c74219bcf931cb31a06ed58f8d27f
juhyoungryu <thinkoutside2004@gmail.com>
2022-06-20 01:13:32 +0900

    꽃병만들기 시작

--- src/controllers/onController.js
+++ src/controllers/onController.js
@@ -2,10 +2,6 @@ export const profile = (req, res) => {
   res.send(`유저: ${req.params.user}`);
 };
 
-export const controlPot = (req, res) => {
-  res.send("꽃병 관리자");
-};
-
 export const setting = (req, res) => {
   res.send(`유저: ${req.params.user} 꽃병: ${req.params.id}`);
 };
--- src/routers/onRouter.js
+++ src/routers/onRouter.js
@@ -2,7 +2,6 @@ import express from "express";
 import {
   profile,
   setting,
-  controlPot,
   write,
   createLetter,
 } from "../controllers/onController";
@@ -11,9 +10,8 @@ const onRouter = express.Router();
 
 onRouter.get("/:user", profile);
 // onRouter.get("/:user", profile);
-onRouter.get("/:user/:FB_id", controlPot);
 onRouter.get("/:user/:FB_id/setting", setting);
 onRouter.get("/:user/:FB_id/write", write);
-onRouter.route("/create/:user").get(createLetter);
+onRouter.route("/:user/create").get(createLetter);
 
 export default onRouter;
