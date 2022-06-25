fdf745c5ae278ff673663594bf13e1448118dbd4
juhyoungRyu <thinkoutside2004@gmail.com>
2022-06-26 05:26:06 +0900

    test

--- src/controllers/onController.js
+++ src/controllers/onController.js
@@ -9,19 +9,3 @@ export const setting = (req, res) => {
 export const write = (req, res) => {
   res.send("편지 추가");
 };
-
-export const createLetter = (req, res) => {
-  let url = req.originalUrl;
-
-  if (url.substr(-1) === "?") {
-    url = url.substr(-2, 1);
-  } else {
-    url = url.substr(-1);
-  }
-  console.log(url);
-  if (url == "1") {
-    return res.render("on/4_1");
-  } else if (url == "2") {
-    return res.render("on/4_2");
-  }
-};
