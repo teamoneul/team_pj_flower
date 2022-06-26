3f6998dabca707e07f0d9348a13e32f3d70971a9
juhyoungRyu <thinkoutside2004@gmail.com>
2022-06-26 23:18:30 +0900

    꽃병 제작 중

--- src/controllers/fbController.js
+++ src/controllers/fbController.js
@@ -1,1 +1,26 @@
 import FlowerBottle from "../models/flowerBottle";
+
+export const getCreate = (req, res) => {
+  let url = req.originalUrl;
+  // url 제일 마지막이 ?일 경우를 대비
+  if (url.substr(-1) === "?") {
+    url = url.substr(-2, 1);
+  } else {
+    url = url.substr(-1);
+  }
+
+  // url 제일 마지막 숫자에 따라 렌더링할 템플릿이 달라진다
+  if (url == "1") {
+    res.render("on/4_1");
+  } else if (url == "2") {
+    res.render("on/4_2");
+  } else {
+    res.render("on/4_3");
+  }
+
+  console.log(req.body);
+};
+
+export const postCreate = (req, res) => {
+  console.log("POST!");
+};
--- src/routers/onRouter.js
+++ src/routers/onRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createLetter } from "../controllers/letterController";
+import { getCreate, postCreate } from "../controllers/fbController";
 import { profile, setting, write } from "../controllers/onController";
 
 const onRouter = express.Router();
@@ -8,6 +8,6 @@ onRouter.get("/:user", profile);
 // onRouter.get("/:user", profile);
 onRouter.get("/:user/:FB_id/setting", setting);
 onRouter.get("/:user/:FB_id/write", write);
-onRouter.route("/:user/create/:stage").get(createLetter);
+onRouter.route("/:user/create/:stage").get(getCreate).post(postCreate);
 
 export default onRouter;
