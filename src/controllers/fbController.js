eb214e4978dea9891fc9aa26952d01256ebf77c3
juhyoungRyu <thinkoutside2004@gmail.com>
2022-06-27 02:24:30 +0900

    꽃병 중복 문제 해결

--- src/controllers/fbController.js
+++ src/controllers/fbController.js
@@ -1,26 +1,45 @@
+import mongoose from "mongoose";
 import FlowerBottle from "../models/flowerBottle";
 
 export const getCreate = (req, res) => {
-  let url = req.originalUrl;
-  // url 제일 마지막이 ?일 경우를 대비
-  if (url.substr(-1) === "?") {
-    url = url.substr(-2, 1);
-  } else {
-    url = url.substr(-1);
-  }
+  res.render("on/4_1");
+};
 
-  // url 제일 마지막 숫자에 따라 렌더링할 템플릿이 달라진다
-  if (url == "1") {
-    res.render("on/4_1");
-  } else if (url == "2") {
-    res.render("on/4_2");
-  } else {
-    res.render("on/4_3");
-  }
+let bottleData = {};
+// bottleData[_id] = {};
 
-  console.log(req.body);
+const create_ID = (id) => {
+  bottleData[id] = {};
 };
 
 export const postCreate = (req, res) => {
-  console.log("POST!");
+  let url = req.originalUrl;
+  url = url.substr(-1);
+  const _id = req.params.user;
+
+  // url 제일 마지막 숫자에 따라 렌더링할 템플릿이 달라진다
+  if (url == "2") {
+    res.render("on/4_2");
+    create_ID(_id);
+  } else if (url == "3") {
+    res.render("on/4_3");
+  }
+
+  const { name, month, day } = req.body;
+  if (name) {
+    bottleData[_id].name = name;
+    console.log(bottleData);
+  }
+  if (month && day) {
+    bottleData[_id].month = month;
+    bottleData[_id].day = day;
+    console.log(bottleData);
+  }
+
+  if (url == "4") {
+    res.send(
+      `${bottleData[_id].name}님의 꽃병, 축하 날짜는 ${bottleData[_id].month}월 ${bottleData[_id].day}일`
+    );
+    console.log(bottleData);
+  }
 };
